d569f1d9c38da274d9a2c2f99e2a9314e053b5c6
Param45 <152694332+Param45@users.noreply.github.com>
2025-09-29 16:43:15 +0530

    Update M&C_BFS.py

--- Lab1/M&C_BFS.py
+++ Lab1/M&C_BFS.py
@@ -1,31 +1,37 @@
 from collections import deque
+
+# Validate a state
 def is_valid(state):
     M_left, C_left, boat = state
     M_right, C_right = 3 - M_left, 3 - C_left
 
-    # Check for invalid ranges
+    # Check ranges
     if not (0 <= M_left <= 3 and 0 <= C_left <= 3):
         return False
 
-    # Missionaries are not eaten on left bank
+    # Safety rule on left bank
     if M_left > 0 and C_left > M_left:
         return False
 
-    # Missionaries are not eaten on right bank
+    # Safety rule on right bank
     if M_right > 0 and C_right > M_right:
         return False
 
     return True
 
+
+# Generate all valid successor states
 def get_successors(state):
     M_left, C_left, boat = state
     successors = []
-    moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]  # possible moves
+
+    # Possible moves: (missionaries, cannibals)
+    moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
 
     for M_move, C_move in moves:
-        if boat == 0:  # Boat on left bank
+        if boat == 0:
             new_state = (M_left - M_move, C_left - C_move, 1)
-        else:  # Boat on right bank
+        else:        
             new_state = (M_left + M_move, C_left + C_move, 0)
 
         if is_valid(new_state):
@@ -33,6 +39,8 @@ def get_successors(state):
 
     return successors
 
+
+# BFS search
 def bfs(start_state, goal_state):
     queue = deque()
     queue.append((start_state, [start_state]))
@@ -55,8 +63,9 @@ def bfs(start_state, goal_state):
 
 
 if __name__ == "__main__":
-    start = (3, 3, 0)  # 3 missionaries, 3 cannibals, boat on left
-    goal = (0, 0, 1)   # all moved to right
+    start = (3, 3, 0)  # boat on left bank
+    goal = (0, 0, 1)   # all moved to right bank
+
     solution = bfs(start, goal)
 
     if solution:
